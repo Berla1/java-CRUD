9f2c938016af1c6ffb2aef0b15ab52c8751ff9fc
Gustavo Berlanga <74972093+Berla1@users.noreply.github.com>
2024-10-19 03:23:43 -0300

    user services sendo usado e dao sendo refeito pq deu ruim no commit

--- src/Main.java
+++ src/Main.java
@@ -1,16 +1,11 @@
-import java.util.Scanner;
 
 public class Main {
     public static void main(String[] args) {
-        Scanner scanner = new Scanner(System.in);
-        User u = new User();
+        UserService userService = new UserService();
 
-        System.out.print("Digite o nome do usuario que deseja cadastrar: ");
-        u.setNome(scanner.next());
-        System.out.print("Digite a idade do usuario que deseja cadastrar: ");
-        u.setIdade(scanner.nextInt());
-
-        new UserDAO().cadastrarUser(u);
+        userService.setUser();
+        userService.getUser();
+        userService.updateUser();
     }
 
 
--- src/UserDAO.java
+++ src/UserDAO.java
@@ -1,5 +1,8 @@
 import java.sql.PreparedStatement;
+import java.sql.ResultSet;
 import java.sql.SQLException;
+import java.util.ArrayList;
+import java.util.List;
 
 public class UserDAO {
     public void cadastrarUser(User user){
@@ -16,4 +19,44 @@ public class UserDAO {
             throw new RuntimeException(e);
         }
     }
+    public List<User> getUsers(){
+        String sql = "SELECT * FROM tb_teste";
+        List<User> users = new ArrayList<User>();
+        PreparedStatement ps = null;
+        ResultSet rset = null;
+        try{
+            ps = Conexao.getConexao().prepareStatement(sql);
+            rset = ps.executeQuery();
+            // Itera pela lista pra fazer o get
+            while(rset.next()){
+                User user = new User();
+                user.setId(rset.getInt("id"));
+                user.setNome(rset.getString("nome"));
+                user.setIdade(rset.getInt("idade"));
+                users.add(user);
+            }
+            ps.close();
+        } catch (SQLException e) {
+            throw new RuntimeException(e);
+        }
+        return users;
+    }
+
+    public void updateUser(User user) {
+        String sql = "UPDATE tb_teste SET nome = ?, idade = ? WHERE id = ?";
+
+        PreparedStatement ps = null;
+
+        try {
+            ps = Conexao.getConexao().prepareStatement(sql);
+            ps.setString(1, user.getNome());
+            ps.setInt(2, user.getIdade());
+            ps.setInt(3, user.getId());
+            ps.execute();
+            ps.close();
+        } catch (Exception e) {
+            throw new RuntimeException(e);
+        }
+    }
+
 }
